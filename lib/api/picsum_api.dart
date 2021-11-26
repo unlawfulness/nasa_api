ea154ecf9f59aa9cb56a4a47fe8000a0d43702dd
unlawfulness <ilmikhaylin@gmail.com>
2021-11-26 15:55:53 +0300

    1

--- lib/api/picsum_api.dart
+++ lib/api/picsum_api.dart
@@ -6,7 +6,7 @@ import 'package:nasa_api/models/picsum_image.dart';
 
 class PicsumApi{
   static Future<List<PicsumImage>> getPicsumImage(BuildContext context) async{
-
+    //max page 34 
     const apiURL = 'https://picsum.photos/v2/list?page=2&limit=100'; 
     var url = Uri.parse(apiURL);
     var response = await http.get(url);
